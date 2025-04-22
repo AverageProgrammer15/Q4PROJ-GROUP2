940ae4a7e8067905244bd8e9d79d946f9402c5cc
Daradal-9T <bananaddict21@gmail.com>
2025-04-22 11:12:35 +0800

    main.js
    Added functions for the feedback form.
    
    Explanation:
    
    checkifcomplete() - check each id input if it has a value and return either true or false
    
    submit() - broadcasts the results of checkifcomplete()

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
+function checkifcomplete(ids){
+    Finished = true
+    for (i = 0; i < ids.length; i++){
+        InputUser = document.getElementById(ids[i]).value;
+        if (InputUser.length  == 0){
+            Finished = false
+        }
+    }
 
+    return Finished
+}
+
+function submit(outputid){
+    Status = checkifcomplete(["id1", "id2", "id3"])
+
+    Result = "N/A"
+    if (Status == true){
+        Result = "Form submitted, thank you for filling it out"
+    } else{
+        Result = "Please finish the whole form"
+    }
+    
+
+    try{
+        if (Status == true){
+            document.getElementById(outputid).innerHTML = Result
+        } else{
+            document.getElementById(outputid).innerHTML = "<span class='text-danger'>" + Result + "</span>"
+        }
+    } catch{
+        window.alert(Result)
+    }
+}
